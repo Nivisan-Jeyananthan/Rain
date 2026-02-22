00cbeae175f7344a31ed7ff393ad29ae02e9f6b8
Nivisan-Jeyananthan <Nivisan.Jeyananthan@gmx.ch>
2026-02-22 23:34:43 +0100

    feat(Rain.Serialization): fixes read long + adds documentation

--- RainCloud.Serialization/src/ch/nivisan/raincloud/serialization/SerializationWriter.java
+++ RainCloud.Serialization/src/ch/nivisan/raincloud/serialization/SerializationWriter.java
@@ -3,200 +3,283 @@ package ch.nivisan.raincloud.serialization;
 import java.util.Random;
 
 /**
- * rESPONIBLE FOR INTERACTING WITH LOWEST LEVEL OF OPERATIONS.
- * IDEALLY WRITES THE FORMAT ITSELF.
+ * responible for interacting with lowest level of operations.
+ * ideally writes the format itself.
  * 
- * USER GIVES STRINGS OR OTHER OBJ DATA, IT GETS CONVERTED INTO BYTES
- * IS NOT RESPONSIBLE FOR INPUT/OUTPUT
+ * user gives strings or other obj data, it gets converted into bytes
+ * is not responsible for input/output
  */
 
 public class SerializationWriter {
 	public static final byte[] headerName = "RC".getBytes();
 	public static final short version = 0x0100; // big-endian
-	
-	
-	public static int copyBytes(byte[] destination, int pointer, byte[] source) {		
+
+	/**
+	 * Copies the bytes from 1 array to another using the pointer as a starting
+	 * position
+	 * 
+	 * @param destination into which array the data will be written to
+	 * @param pointer     the starting position at which we will insert our data
+	 * @param source      from where we will take the data
+	 * @return pointer position to the end of our data
+	 */
+	public static int copyBytes(byte[] destination, int pointer, byte[] source) {
+		if (destination.length - 1 < pointer + (source.length))
+			return pointer;
+
 		for (int i = 0; i < source.length; i++) {
 			destination[pointer++] = source[i];
 		}
 		return pointer;
 	}
-	
+
+	/**
+	 * Writes the given float value at the given pointer to the desination array
+	 * 1 byte is 8 bits
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
+	 */
 	public static int writeBytes(byte[] destination, int pointer, byte value) {
 		destination[pointer++] = value;
 		return pointer;
 	}
-	
-	/**
-	 * Shorts are 16bit
-	 */
-	public static int writeBytes(byte[] destination, int pointer, short value) {
-		destination[pointer++] = (byte)((value >> 8) & 0xff);
-		destination[pointer++] = (byte)((value >> 0) & 0xff);
-		
-		return pointer;
-	}
-	
 
 	/**
-	 * Char is unsinged Short 16 bit
-	 * @param destination
-	 * @param pointer
-	 * @param value
-	 * @return pointer to next location
-	 */ 
-	public static int writeBytes(byte[] destination, int pointer, char value) {
-		destination[pointer++] = (byte)((value >> 8) & 0xff);
-		destination[pointer++] = (byte)((value >> 0) & 0xff);
-		
+	 * Writes the given float value at the given pointer to the desination array
+	 * short are 16 bit
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
+	 */
+	public static int writeBytes(byte[] destination, int pointer, short value) {
+		destination[pointer++] = (byte) ((value >> 8) & 0xff);
+		destination[pointer++] = (byte) ((value >> 0) & 0xff);
+
 		return pointer;
 	}
-	
+
 	/**
-	 * Integers are 32bit
-	 * @param destination
-	 * @param pointer
-	 * @param value
-	 * @return pointer to next location
+	 * Writes the given float value at the given pointer to the desination array
+	 * Char is in java a unsinged short 16 bit
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
+	 */
+	public static int writeBytes(byte[] destination, int pointer, char value) {
+		destination[pointer++] = (byte) ((value >> 8) & 0xff);
+		destination[pointer++] = (byte) ((value >> 0) & 0xff);
+
+		return pointer;
+	}
+
+	/**
+	 * Writes the given float value at the given pointer to the desination array
+	 * integers are 32 bit
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
 	 */
 	public static int writeBytes(byte[] destination, int pointer, int value) {
-		destination[pointer++] = (byte)((value >> 24) & 0xff);
-		destination[pointer++] = (byte)((value >> 16) & 0xff);
-		destination[pointer++] = (byte)((value >> 8) & 0xff);
-		destination[pointer++] = (byte)((value >> 0) & 0xff);
-		
+		destination[pointer++] = (byte) ((value >> 24) & 0xff);
+		destination[pointer++] = (byte) ((value >> 16) & 0xff);
+		destination[pointer++] = (byte) ((value >> 8) & 0xff);
+		destination[pointer++] = (byte) ((value >> 0) & 0xff);
+
 		return pointer;
 	}
-	
+
 	/**
-	 * long are 64bit values
-	 * @param destination
-	 * @param pointer
-	 * @param value
-	 * @return pointer to next location
+	 * Writes the given float value at the given pointer to the desination array
+	 * long are 64 bit
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
 	 */
 	public static int writeBytes(byte[] destination, int pointer, long value) {
-		destination[pointer++] = (byte)((value >> 56) & 0xff);
-		destination[pointer++] = (byte)((value >> 48) & 0xff);
-		destination[pointer++] = (byte)((value >> 40) & 0xff);
-		destination[pointer++] = (byte)((value >> 32) & 0xff);
-		destination[pointer++] = (byte)((value >> 24) & 0xff);
-		destination[pointer++] = (byte)((value >> 16) & 0xff);
-		destination[pointer++] = (byte)((value >> 8) & 0xff);
-		destination[pointer++] = (byte)((value >> 0) & 0xff);
-		
+		destination[pointer++] = (byte) ((value >> 56) & 0xff);
+		destination[pointer++] = (byte) ((value >> 48) & 0xff);
+		destination[pointer++] = (byte) ((value >> 40) & 0xff);
+		destination[pointer++] = (byte) ((value >> 32) & 0xff);
+		destination[pointer++] = (byte) ((value >> 24) & 0xff);
+		destination[pointer++] = (byte) ((value >> 16) & 0xff);
+		destination[pointer++] = (byte) ((value >> 8) & 0xff);
+		destination[pointer++] = (byte) ((value >> 0) & 0xff);
+
 		return pointer;
 	}
-	
-	
+
+	/**
+	 * Writes the given float value at the given pointer to the desination array
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
+	 */
 	public static int writeBytes(byte[] destination, int pointer, float value) {
 		int data = Float.floatToIntBits(value);
 
-		return writeBytes(destination,pointer,data);
+		return writeBytes(destination, pointer, data);
 	}
-	
+
 	public static int writeBytes(byte[] destination, int pointer, double value) {
 		double data = Double.doubleToLongBits(value);
-		
-		return writeBytes(destination,pointer,data);
+
+		return writeBytes(destination, pointer, data);
 	}
-	
+
 	/**
+	 * Writes the given float value at the given pointer to the desination array
 	 * Boolean = 1 Byte
-	 * @param destination
-	 * @param pointer
-	 * @param value
-	 * @return pointer to next location
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
 	 */
 	public static int writeBytes(byte[] destination, int pointer, boolean value) {
-		
-		destination[pointer++] = (byte)  (value ? 1 : 0);
-		
+
+		destination[pointer++] = (byte) (value ? 1 : 0);
+
 		return pointer;
 	}
-	
-	
+
 	/**
-	 * Boolean = 1 Byte
-	 * TODO: test and rewrite for arrays
-	 * @param destination
-	 * @param pointer
-	 * @param values
-	 * @return pointer to next location
+	 * Writes the given float value at the given pointer to the desination array
+	 * Boolean = 1 Byte = 8 bit
+	 * Trying to put multiple booleans into 1 Byte, since the actual data is only 1
+	 * Bit
+	 * TODO: test and rewrite for array works in theory
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer position to the end of our data
 	 */
 	public static int writeBytes(byte[] destination, int pointer, boolean[] values) {
-		
-		  byte bitfield = 0;
-		    
-		    for (int i = 0; i < values.length -1; i++) {
-		 	   System.out.println(values[i]);
-		    	}
-		    int max = values.length >= 8 ? 8 : values.length -1;
-		    
-		    for (int i = 0; i < max; i++) {
-		 	   bitfield |=  (values[i] ? 1 : 0) << i;
-		    }
-		    
-		    destination[pointer++] = bitfield;
-		    System.out.println(bitfield);
-		
+		byte bitfield = 0;
+		int max = values.length >= 8 ? 8 : values.length - 1;
+
+		for (int i = 0; i < max; i++) {
+			bitfield |= (values[i] ? 1 : 0) << i;
+		}
+		destination[pointer++] = bitfield;
+
 		return pointer;
 	}
-	 
-  
+
 	/**
-	 * 	// 2 options: 
-		// 1. Write the size of the string before the string as a short ( 07 NI VI SA N).
-		// bytes of 2 for size so we are not limited to 255 characters but have plenty of space
-		// 2. Null-termination character (binary 0) at the end: (NI VI SA N0).
-		// it is not the same as the character 0
-	 * @param destination
-	 * @param pointer
-	 * @param value
-	 * @return
+	 * 2 options:
+	 * 1. Write the size of the string before the string as a short ( 07 NI VI SA
+	 * bytes of 2 for size so we are not limited to 255 characters but have
+	 * plenty of space
+	 * 2. Null-termination character (binary 0) at the end: (NI VI SA N0).
+	 * it is not the same as the character 0
+	 * 
+	 * @param destination the byte array to write to
+	 * @param pointer     at which position the given value should be inserted to
+	 * @param value       the value to insert
+	 * @return pointer to next location
 	 */
 	public static int writeBytes(byte[] destination, int pointer, String value) {
-		pointer = writeBytes(destination, pointer, (short)value.length());
+		pointer = writeBytes(destination, pointer, (short) value.length());
 		return copyBytes(destination, pointer, value.getBytes());
 	}
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as byte
+	 */
 	public static byte readByte(byte[] source, int pointer) {
 		return source[pointer];
 	}
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as char
+	 */
 	public static char readChar(byte[] source, int pointer) {
-		return (char)  (source[pointer] << 8 | source[pointer+1]);
+		return (char) (source[pointer] << 8 | source[pointer + 1]);
 	}
-	
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as short
+	 */
 	public static short readShort(byte[] source, int pointer) {
-		return (short)  (source[pointer] << 8 | source[pointer+1]);
+		return (short) (source[pointer] << 8 | source[pointer + 1]);
 	}
-	
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as int
+	 */
 	public static int readInt(byte[] source, int pointer) {
-		return source[pointer] << 24 | source[pointer+1] << 16 | source[pointer+2] << 8 | source[pointer+3] << 0;
+		return source[pointer] << 24 | source[pointer + 1] << 16 | source[pointer + 2] << 8 | source[pointer + 3] << 0;
 	}
-	
-	public static int readLong(byte[] source, int pointer) {
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as long
+	 */
+	public static long readLong(byte[] source, int pointer) {
 		return source[pointer] << 56 |
-				source[pointer+1] << 48 |
-				source[pointer+2] << 40 |
-				source[pointer+3] << 32 |
-				source[pointer+4] << 24 | 
-				source[pointer+5] << 16 | 
-				source[pointer+6] << 8 |
-				source[pointer+7] << 0;
+				source[pointer + 1] << 48 |
+				source[pointer + 2] << 40 |
+				source[pointer + 3] << 32 |
+				source[pointer + 4] << 24 |
+				source[pointer + 5] << 16 |
+				source[pointer + 6] << 8 |
+				source[pointer + 7] << 0;
 	}
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as float
+	 */
 	public static float readFloat(byte[] source, int pointer) {
 		return Float.intBitsToFloat(readInt(source, pointer));
 	}
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as double
+	 */
 	public static double readDouble(byte[] source, int pointer) {
 		return Double.longBitsToDouble(readLong(source, pointer));
 	}
-	
+
+	/**
+	 * Read data at said pointer position
+	 * @param source from where to read the data
+	 * @param pointer at what position our data begins
+	 * @return our data as boolean
+	 */
 	public static boolean readBoolean(byte[] source, int pointer) {
 		return source[pointer] != 0;
 	}
