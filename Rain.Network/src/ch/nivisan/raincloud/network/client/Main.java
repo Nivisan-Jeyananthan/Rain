b04a47d97bf71174b831e16bc9bbead62381b9d8
Nivisan-Jeyananthan <Nivisan.Jeyananthan@gmx.ch>
2026-04-06 20:47:00 +0200

    fix(Rain.Network): publicing of main

--- Rain.Network/src/ch/nivisan/raincloud/network/client/Main.java
+++ Rain.Network/src/ch/nivisan/raincloud/network/client/Main.java
@@ -2,18 +2,18 @@ package ch.nivisan.raincloud.network.client;
 
 import java.awt.EventQueue;
 
-class Main {
+public class Main {
 
-    static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new Login();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					new Login();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 }
